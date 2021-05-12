0854976de383e3010dcd4d275386a62b881a6d58
simplebunsen <52760596+simplebunsen@users.noreply.github.com>
2021-05-12 23:31:02 +0200

    first attempt at completed algorithm, likely not working

--- cipherer.rb
+++ cipherer.rb
@@ -6,22 +6,37 @@ def caesar_cipher(input, shift) do
 
     input_val = character.ord
 
-    #check if original char is char
+    #check if original char is !letter
     if ((0..64) === input_val || (91..96) === input_val || (123..127) === input_val) do
       output.concat(character) 
       next
     end
 
-    #save whether original char was upcase so we can return the result to it later
+    #save whether the char is upcase for later restoration
     upcase? = character.upcase == character
 
+    #standardize all letters to lower case to simplify process
+    input_val = character.downcase.ord
+
+    #do the shift
     shifted_val = input_val + shift
 
-
+    #check for if the shift made it go out of bounds and correct it
+    until in_range? do
+      case
+      when shifted_val > 122
+        shifted_val -= 26
+      when shifted_val < 97
+        shifted_val += 26
+      end
+    end
 
     if upcase? {shifted_val = shifted_val.upcase}
     output.concat(shifted_val.chr)   
   end
-
   return output
+end
+
+def in_range?(val)
+  return (97..122) === val
 end
